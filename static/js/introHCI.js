6d6c06d55b48095ec7378617321594a3671c71b1
Kyle Zhu <kyzhu@ucsd.edu>
2016-01-20 22:16:29 -0800

    save

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,39 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Help me I've fallen and I can't get up");
+		$("#testjs").text("what is this halp");
+		$(".jumbotron p").toggleClass("active");
+
+
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(updateProject);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(".project-description").fadeToggle();
+    }
+}
+
+function updateProject(e) {
+	var ID = $("#project").val();
+	$(ID).animate({
+		width: $("#width").val()
+	});
+
+	var hello = $("#description").val();
+	$(ID + " .project-description").text(hello);
 }
